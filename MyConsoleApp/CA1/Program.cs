9596c137f48c7a2bacf46a487a5aabc2d4477afa
smcilvenny <s.mcilvenny@students.clark.edu>
2019-11-21 02:07:58 -0800

    Console App/Command Line assignments I did the bonus also

---
+++ MyConsoleApp/CA1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("several lines of text");
+            //Console.WriteLine("several lines of more text");
+            //Console.WriteLine("and even more lines of text");
+            
+            string[] lines = File.ReadAllLines(@"D:\CTEC\CONSOLEAPPS\console-apps-smcilvenny\MyConsoleApp\bonus.txt");
+            args = lines;
+
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("There are 0 args");
+            }
+            else
+            {
+                Console.WriteLine($"there are {args.Length} arguments");
+            }
+
+            foreach(string s in args){
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
